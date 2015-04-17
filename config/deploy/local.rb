0171968d50ce68df5c7194d43eaa0d33ac2c8061
Tristan Dunn <tristanzdunn@gmail.com>
2015-04-16 20:00:14 -0500

    Ensure URLs are unique between development, staging, and production.

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -2,4 +2,5 @@ require "./config/deploy/local/file_strategy"
 
 server "vagrant", user: "deploy", roles: %w(web)
 
-set :git_strategy, FileStrategy
+set :git_strategy,  FileStrategy
+set :configuration, "_config.yml,_config_staging.yml"
