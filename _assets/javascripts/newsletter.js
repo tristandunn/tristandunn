ea2e1ac695c06fbb894d254ea7200cf16ec3a525
Tristan Dunn <tristanzdunn@gmail.com>
2015-06-22 20:11:19 -0500

    Reduce size of JS for newsletter forms.

--- _assets/javascripts/newsletter.js
+++ _assets/javascripts/newsletter.js
@@ -1,7 +1,15 @@
 (function() {
-  if (!document.cookie.match("subscribed=true")) {
+  if (document.cookie.match("subscribed=true")) {
+    document.querySelector("body").classList.add("subscribed");
+
     return;
   }
 
-  document.querySelector("body").classList.add("subscribed");
+  document.querySelector(".newsletter form").addEventListener("submit", function() {
+    var time = new Date();
+
+    time.setMilliseconds(time.getMilliseconds() + 365 * 86400000);
+
+    document.cookie = "subscribed=true; path=/; expires=" + time.toUTCString();
+  }, false);
 })();
