6498292b9a65821936991f4999a36b6566bee29d
Tristan Dunn <tristanzdunn@gmail.com>
2015-12-18 23:51:38 -0600

    Correct local deployment server.

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -1,6 +1,6 @@
 require "./config/deploy/local/file_strategy"
 
-server "vagrant", user: "deploy", roles: %w(web)
+server "tristandunn.local", user: "deploy", roles: %w(web)
 
 set :git_strategy,  FileStrategy
 set :configuration, "_config.yml,_config_staging.yml"
