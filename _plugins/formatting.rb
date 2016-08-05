97fc39eaa3ae5065cac8478883bec443b786b5c3
Tristan Dunn <tristanzdunn@gmail.com>
2016-08-05 13:37:47 -0500

    Avoid widows in titles and subtitles.

--- _plugins/formatting.rb
+++ _plugins/formatting.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/inflections"
 
 module Jekyll
   module Filters
+    NON_BREAKING_SPACE = "&#160;".freeze
+
     def date_to_string(date, format = "%B %o, %Y")
       date   = time(date)
       format = format.sub("%o", date.day.ordinalize)
@@ -12,6 +14,10 @@ module Jekyll
 
     def titleize(string)
       string.titleize
+    end
+
+    def widow(string)
+      string.gsub(/(.*)(\s)(.*)/, "\\1#{NON_BREAKING_SPACE}\\3")
     end
   end
 end
