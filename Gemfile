944fe94bf3f7433b354ac90bd766fc685f0aace6
Tristan Dunn <tristanzdunn@gmail.com>
2021-04-07 23:41:29 -0400

    Update dependencies.

--- Gemfile
+++ Gemfile
@@ -12,14 +12,14 @@ group :jekyll_plugins do
 end
 
 group :development do
-  gem "bundler-audit",       "0.7.0.1", require: false
-  gem "mdl",                 "0.11.0",  require: false
-  gem "rack",                "2.2.3",   require: false
-  gem "rubocop",             "1.10.0",  require: false
-  gem "rubocop-performance", "1.9.2",   require: false
-  gem "scss_lint",           "0.59.0",  require: false
+  gem "bundler-audit",       "0.8.0",  require: false
+  gem "mdl",                 "0.11.0", require: false
+  gem "rack",                "2.2.3",  require: false
+  gem "rubocop",             "1.12.1", require: false
+  gem "rubocop-performance", "1.10.2", require: false
+  gem "scss_lint",           "0.59.0", require: false
 end
 
 group :test do
-  gem "html-proofer", "3.18.6"
+  gem "html-proofer", "3.18.8"
 end
--- _plugins/kramdown/highlighter.rb
+++ _plugins/kramdown/highlighter.rb
@@ -2,116 +2,120 @@
 
 require "rouge"
 
-class Highlighter < Rouge::Formatters::HTMLLinewise
-  SPACE                 = " "
-  BLANK_LINE            = [[Rouge::Token::Tokens::Text, "\n"].freeze].freeze
-  CLASS_FORMAT          = "line line-%<line>i"
-  HIGHLIGHT_CLASS       = "highlight"
-  RANGE_SEPARATOR       = "-"
-  PARAGRAPH_MATCHER     = %r{</?p>}.freeze
-  HIGHLIGHT_LINE_CLASS  = "highlight-line"
-  HIGHLIGHT_LINES_CLASS = "highlight-lines"
+module Rouge
+  module Formatters
+    class CustomHighlighter < HTMLLinewise
+      SPACE                 = " "
+      BLANK_LINE            = [[Rouge::Token::Tokens::Text, "\n"].freeze].freeze
+      CLASS_FORMAT          = "line line-%<line>i"
+      HIGHLIGHT_CLASS       = "highlight"
+      RANGE_SEPARATOR       = "-"
+      PARAGRAPH_MATCHER     = %r{</?p>}.freeze
+      HIGHLIGHT_LINE_CLASS  = "highlight-line"
+      HIGHLIGHT_LINES_CLASS = "highlight-lines"
 
-  attr_reader :formatter, :options
+      attr_reader :formatter, :options
 
-  # Create a new highlighter.
-  #
-  # @param options [Hash] The options for highlighting.
-  # @option options [String] :lines The specific lines to call out.
-  # @option options [String] :caption The figure caption.
-  def initialize(options = {})
-    @options = options.transform_keys(&:to_sym)
+      # Create a new highlighter.
+      #
+      # @param options [Hash] The options for highlighting.
+      # @option options [String] :lines The specific lines to call out.
+      # @option options [String] :caption The figure caption.
+      def initialize(options = {})
+        @options = options.transform_keys(&:to_sym)
 
-    super(Rouge::Formatters::HTML.new, @options)
-  end
-
-  # Builds a figure caption if a caption option is present.
-  #
-  # @return [String|nil]
-  def caption
-    return if options[:caption].to_s.strip.empty?
-
-    html = Kramdown::Document
-           .new(options[:caption])
-           .to_html
-           .gsub(PARAGRAPH_MATCHER, "")
-           .strip
-
-    "<figcaption>#{html}</figcaption>"
-  end
-
-  # Builds necessary CSS class names for the container.
-  #
-  # @return [String]
-  def container_class_names
-    class_names = [HIGHLIGHT_CLASS]
-    class_names << HIGHLIGHT_LINES_CLASS if lines.any?
-    class_names.join(SPACE)
-  end
-
-  # Builds necessary CSS class names for the next line of code.
-  #
-  # @param line [Number] The line number the class names are for.
-  # @return [String]
-  def line_class_names(line)
-    class_names = [Kernel.format(CLASS_FORMAT, line: line)]
-    class_names << HIGHLIGHT_LINE_CLASS if lines.include?(line)
-    class_names.join(SPACE)
-  end
-
-  # Convert line numbers and ranges to an array of numbers.
-  #
-  # @param value [String] The line numbers and ranges to parse.
-  # @return [Array]
-  def lines
-    @lines ||= options.fetch(:lines, nil)
-                      .to_s
-                      .split(SPACE)
-                      .flat_map do |item|
-                        item = item.split(RANGE_SEPARATOR, 2)
-                        item = Range.new(*item) if item.size == 2
-                        item.to_a
-                      end
-                      .map(&:to_i)
-  end
-
-  # Stream the highlighted tokens to the provided block.
-  #
-  # @param tokens [Array] The tokens to highlight.
-  # @return [void]
-  def stream(tokens, &_block)
-    yield stream_header
-
-    token_lines(tokens).with_index do |line, index|
-      yield %(<div class="#{line_class_names(index + 1)}">)
-
-      line = BLANK_LINE if line.empty?
-      line.each do |token, value|
-        yield formatter.span(token, value)
+        super(Rouge::Formatters::HTML.new, @options)
       end
 
-      yield %(</div>)
+      # Builds a figure caption if a caption option is present.
+      #
+      # @return [String|nil]
+      def caption
+        return if options[:caption].to_s.strip.empty?
+
+        html = Kramdown::Document
+               .new(options[:caption])
+               .to_html
+               .gsub(PARAGRAPH_MATCHER, "")
+               .strip
+
+        "<figcaption>#{html}</figcaption>"
+      end
+
+      # Builds necessary CSS class names for the container.
+      #
+      # @return [String]
+      def container_class_names
+        class_names = [HIGHLIGHT_CLASS]
+        class_names << HIGHLIGHT_LINES_CLASS if lines.any?
+        class_names.join(SPACE)
+      end
+
+      # Builds necessary CSS class names for the next line of code.
+      #
+      # @param line [Number] The line number the class names are for.
+      # @return [String]
+      def line_class_names(line)
+        class_names = [Kernel.format(CLASS_FORMAT, line: line)]
+        class_names << HIGHLIGHT_LINE_CLASS if lines.include?(line)
+        class_names.join(SPACE)
+      end
+
+      # Convert line numbers and ranges to an array of numbers.
+      #
+      # @param value [String] The line numbers and ranges to parse.
+      # @return [Array]
+      def lines
+        @lines ||= options.fetch(:lines, nil)
+                          .to_s
+                          .split(SPACE)
+                          .flat_map do |item|
+                            item = item.split(RANGE_SEPARATOR, 2)
+                            item = Range.new(*item) if item.size == 2
+                            item.to_a
+                          end
+                          .map(&:to_i)
+      end
+
+      # Stream the highlighted tokens to the provided block.
+      #
+      # @param tokens [Array] The tokens to highlight.
+      # @return [void]
+      def stream(tokens, &_block)
+        yield stream_header
+
+        token_lines(tokens).with_index(1) do |line, index|
+          yield %(<div class="#{line_class_names(index)}">)
+
+          line = BLANK_LINE if line.empty?
+          line.each do |token, value|
+            yield formatter.span(token, value)
+          end
+
+          yield %(</div>)
+        end
+
+        yield stream_footer
+      end
+
+      private
+
+      # Return the footer for the highlighter stream.
+      #
+      # @return [string]
+      def stream_footer
+        %(</code></pre></div>#{caption}</figure>)
+      end
+
+      # Return the header for the highlighter stream.
+      #
+      # @return [string]
+      def stream_header
+        %(
+          <figure>
+            <div class="#{container_class_names}" data-language="#{options[:language]}">
+              <pre><code>)
+      end
     end
-
-    yield stream_footer
-  end
-
-  private
-
-  # Return the footer for the highlighter stream.
-  #
-  # @return [string]
-  def stream_footer
-    %(</code></pre></div>#{caption}</figure>)
-  end
-
-  # Return the header for the highlighter stream.
-  #
-  # @return [string]
-  def stream_header
-    %(
-      <figure>
-        <div class="#{container_class_names}" data-language="#{options[:language]}">
-          <pre><code>)
   end
 end
